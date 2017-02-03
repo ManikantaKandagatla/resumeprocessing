213db13ee536be1827c5701ba30200d6360e11e1
manikanta_kandagatla <manikantakandagatla@gmail.com>
2017-02-03 23:45:46 +0530

    init commit of schedule Interview API

---
+++ src/main/java/com/mywork/resumeprocessing/controller/InterviewSchedulerController.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.mywork.resumeprocessing.controller;
+
+import java.util.List;
+
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mywork.resumeprocessing.model.BulkScheduledInterviews;
+
+/**
+ * @author ManiKanta Kandagatla
+ *
+ */
+@RestController
+public class InterviewSchedulerController {
+
+	private static final Logger log = LoggerFactory.getLogger(InterviewerController.class);
+	
+	@RequestMapping(value = "/scheduleInterviews", method = RequestMethod.POST)
+	public void scheduleInterview(@RequestParam("scheduledInterviews") String scheduleInterview)throws Exception
+	{
+		//boolean success = true;
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		BulkScheduledInterviews scheduledInterviews = mapper.readValue(scheduleInterview, BulkScheduledInterviews.class);
+		
+	}
+	
+}
